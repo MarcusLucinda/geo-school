9e1065ff26cf0a102beb8c866e35867322b8d9ff
MarcusLucinda <marcusvbl@hotmail.com>
2021-06-03 14:24:20 -0300

    initial commit

---
+++ src/main/java/com/lucinda/GeoSchool/GeoSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.lucinda.GeoSchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucinda/GeoSchool/GeoSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucinda.GeoSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
